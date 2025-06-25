3a281f86ce0e6160e83c8c8518429854c0b773e2
handeener <hande.bucaga@gmail.com>
2025-06-26 01:12:36 +0300

    First commit

---
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -0,0 +1,28 @@
+package com.example.employee.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String department;
+}
