795d50b334e44940a1c75271d0f4036f99a40f5b
handeener <hande.bucaga@gmail.com>
2025-08-16 23:42:44 +0300

    refactor employee entity

--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -16,12 +16,10 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String firstName;
 
     private String lastName;
 
-    @Column(unique = true, nullable = false)
     private String email;
 
     private String department;
